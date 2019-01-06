5fb98b4c51c7e211b40e56804ad330d453b6c5b9
Andrey Koltsov <a99920641@gmail.com>
2019-01-06 17:52:03 +0300

    Remove project copy.

--- benchmark/src/main/java/benchmark/files/IInsertionsFileLogger.java
+++
@@ -1,8 +0,0 @@
-package benchmark.files;
-
-import java.io.IOException;
-
-public interface IInsertionsFileLogger {
-    // NOTE: Required parameters are: DB name, destination table name, key, measured duration
-    void logOperation(String targetDatabase, String targetTable, String insertedKey, String operationDuration) throws IOException;
-}
