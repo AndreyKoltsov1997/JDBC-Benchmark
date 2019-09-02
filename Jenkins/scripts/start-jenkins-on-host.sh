93471d2b61de29174009e18f7d09e2c338172488
Andrey Koltsov <a99920641@gmail.com>
2019-09-02 16:33:54 +0300

    Add script for Jenkins start up.

---
+++ Jenkins/scripts/start-jenkins-on-host.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# NOTE: User homepath should be configured with respect to the host.
+USER_HOME=/Users/Andrey/
+
+docker run \
+  --rm \
+  -u root \
+  -p 8080:8080 \
+  -v jenkins-data:/var/jenkins_home \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v "$USER_HOME":/home \
+  jenkinsci/blueocean
